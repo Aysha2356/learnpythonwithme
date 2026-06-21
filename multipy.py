0f5a13ea9bc591e3dcef3ba086c4a867d71163a2
Aysha Rakshana <aysharakshana02@gmail.com>
2026-06-21 14:15:39 +0530

    udpated the changes

---
+++ multipy.py
@@ -0,0 +1,6 @@
+def multiply():
+    a=b=10
+    c=a*b
+    print("the multiplication of a and b is",c)
+
+multiply()
---
+++ subtract.py
@@ -0,0 +1,7 @@
+def subtract():
+    a=20 
+    b=40
+    c=b-a
+    print(c)
+
+subtract()
