59504b085963d8950cf8ab6829843745c3de4ecf
Aysha Rakshana <aysharakshana02@gmail.com>
2026-07-08 14:12:31 +0530

    update the changes

---
+++ assignment1.py
@@ -0,0 +1,7 @@
+# This program prints my personal details
+print("******** WELCOME ********")
+print("Name: Aysha")
+print("Age: 20")
+print("Department: CSE")
+print("Learning: Python")
+print("*************************")
