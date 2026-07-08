7a2f8e44267393098051ab3e79dc1351831ae81e
Aysha Rakshana <aysharakshana02@gmail.com>
2026-07-08 14:24:12 +0530

    update the changes

---
+++ day2/swap.py
@@ -0,0 +1,7 @@
+a=int(input("Enter the first number: "))
+b=int(input("Enter the second number: "))
+temp=a
+a=b
+b=temp
+print("After swapping, the first number is:", a)
+print("After swapping, the second number is:", b)
