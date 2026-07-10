7dfcb07916bb50180adbc092dbd6d11f4eafce4f
Aysha Rakshana <aysharakshana02@gmail.com>
2026-07-10 08:23:06 +0530

    update the changes

---
+++ day3/boolean.py
@@ -0,0 +1,17 @@
+print(True)
+print(False)
+
+print(type(True))
+print(type(False))
+
+print(10 > 5)
+print(10 < 5)
+
+print(20 == 20)
+print(20 != 20)
+
+print("Python" == "Python")
+print("Python" == "python")
+
+print(True + True)
+print(True + False)
---
+++ day3/comparion.py
@@ -0,0 +1,14 @@
+print(10 > 5)
+print(10 < 5)
+
+print(20 >= 20)
+print(15 <= 10)
+
+print(100 == 100)
+print(100 != 100)
+
+print("Python" == "Python")
+print("Python" == "python")
+
+print("A" < "B")
+print("Z" > "A")
