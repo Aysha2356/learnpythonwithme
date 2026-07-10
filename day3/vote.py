8a95a37291787d97da98fe7332d64a2aad392654
Aysha Rakshana <aysharakshana02@gmail.com>
2026-07-10 10:12:16 +0530

    update

---
+++ day3/vote.py
@@ -0,0 +1,8 @@
+age = int(input("Enter your age: "))
+
+if age >= 18:
+    print("Congratulations!")
+    print("You are eligible to vote.")
+else:
+    print("Sorry!")
+    print("You are not eligible to vote.")
