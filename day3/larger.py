40a1e2f844c531fc90efb7e07d10e3ece7fa7177
Aysha Rakshana <aysharakshana02@gmail.com>
2026-07-10 09:07:46 +0530

    update the changes

---
+++ day3/larger.py
@@ -0,0 +1,8 @@
+a=int(input("enter a  number"))
+b=int(input("enter a number"))
+if a>b:
+    print("a is greater than b")
+elif a<b:
+    print("a is less than b")
+else:
+    print("a is equal to b")
